55ef7861b72d6d8b03c878311457f7cd1d59e413
zainabyounes339 <zeinabyounes632@gmail.com>
2025-03-19 13:35:07 +0200

    Fixed a bug

--- db.php
+++ db.php
@@ -1,17 +1,15 @@
 <?php
-$host = "localhost";  // اسم السيرفر (في XAMPP أو WAMP يكون localhost)
-$username = "root";   // اسم المستخدم الافتراضي
-$password = "";       // كلمة المرور (تترك فارغة في XAMPP)
-$database = "BlogDB"; // اسم قاعدة البيانات التي أنشأتها
+$host = "localhost";  
+$username = "root";   
+$password = "";       
+$database = "BlogDB"; 
 
-// إنشاء الاتصال بقاعدة البيانات
+
 $conn = new mysqli($host, $username, $password, $database);
 
-// التحقق من نجاح الاتصال
 if ($conn->connect_error) {
     die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
 }
 
-// ضبط الترميز لضمان دعم اللغة العربية
 $conn->set_charset("utf8");
 ?>
